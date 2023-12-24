4aa14f3e750469b38bea5e0dcb09d4c7ec533cab
deizer0k <pavlo.kiperchuk@gmail.com>
2023-12-24 09:39:50 +0200

    007  passed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,34 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var titleTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonChange = findViewById<Button>(R.id.changeButton)
+        titleTextView = findViewById<TextView>(R.id.titleTextView)
+
+        buttonChange.setOnClickListener {
+            titleTextView.text = "I am an Android Developer!"
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(KEY_TEXT , titleTextView.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val text = savedInstanceState.getString(KEY_TEXT, titleTextView.text.toString())
+        titleTextView.text = text
+    }
+    companion object {
+        private const val KEY_TEXT = "key"
     }
 }
