a8683733e349baa466563009e916c80cb072723a
deizerok <pavlo.kiperchuk@gmail.com>
2024-01-12 08:52:40 +0200

    012 passed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,43 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val count = Count.Base(2, 4)
+
+    private var uiState: UiState = UiState.Base("0")
+
+    private lateinit var countTextView: TextView
+    private lateinit var buttonIncrement: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonIncrement = findViewById(R.id.incrementButton)
+        countTextView = findViewById(R.id.countTextView)
+
+        buttonIncrement.setOnClickListener {
+            uiState = count.increment(countTextView.text.toString())
+            uiState.apply(countTextView, buttonIncrement)
+        }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable(UI_STATE_KEY, uiState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        uiState = savedInstanceState.getSerializable(UI_STATE_KEY) as UiState
+        uiState.apply(countTextView, buttonIncrement)
+    }
+
+    companion object {
+        private const val UI_STATE_KEY = "uiStateKey"
     }
 }
