a4ced451e057e836577c704d03c149033548b375
deizerok <pavlo.kiperchuk@gmail.com>
2024-02-27 20:02:00 +0200

    task 020 passed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,40 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var inputEdit:TextInputEditText
+    private lateinit var textView:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById<TextView>(R.id.titleTextView)
+        inputEdit = findViewById<TextInputEditText>(R.id.inputEditText)
+        val button = findViewById<Button>(R.id.actionButton)
+
+        button.setOnClickListener {
+            textView.text = inputEdit.text.toString()
+            inputEdit.text =null
+        }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("KEY", textView.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val text = savedInstanceState.getString("KEY", textView.text.toString())
+        textView.text = text
     }
 }
